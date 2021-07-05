473e2489d4c574d012ee4a212f1b6783cac71970
davidngo123 <ngodavid215@gmail.com>
2021-07-05 12:47:42 -0700

    added in selenium methods

--- stock_api.py
+++ stock_api.py
@@ -1,11 +1,29 @@
-import yfinance as yf
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+
+from bs4 import BeautifulSoup
+def scrape(symbol):  
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.get('https://finance.yahoo.com/quote/'+symbol+'?p='+symbol+'&.tsrc=fin-srch%27')
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
 
 
+def checkStock(stock_name):
+    symbol = yf.Ticker(stock_name)
+    try:
+        test = symbol.info
+        return True
+    except:
+        return False
 
 
-try:
-  msft = yf.Ticker("MSFTTTTT")
-  todayData = msft.history(period='1d')
-  print(todayData['Close'][0])
-except:
-  print("An exception occurred")
+def getStock(stock_name):
+    if(checkStock(stock_name)):
+        symbol = yf.Ticker(stock_name)
+        todayData = symbol.history(period='1d')
+        return todayData['Close'][0]
+    else:
+        return "Does not exist"
+    
--- stock_bot.py
+++ stock_bot.py
@@ -1,61 +1,59 @@
-# Main file that contains majority of the bots functionality 
-
-import slack 
+from slack import RTMClient
+import requests
+from stock_api import *
+from dotenv import load_dotenv
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-from flask import Flask, request, Response
-from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'], '/slack/events', app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+@RTMClient.run_on(event="message")
+def stockbot(**payload):
+    """
+    This function triggers when someone sends
+    a message on the slack
+    """
+    data = payload["data"]
+    web_client = payload["web_client"]
+    bot_id = data.get("bot_id", "")
 
-all_users = []
+    # If a message is not send by the bot
+    if bot_id == "":
+        channel_id = data["channel"]
 
+        # Extracting message send by the user on the slack
+        text = data.get("text", "")
+        text = text.split()
+        response = ""
+        """ 
+        Prints out the list of commands that's avalible to the user
+        """
+        if "help" in text[1]:
+            user = data.get("user", "")
+            response = f"Hello <@{user}>! You look like you need some help :wave:\n"
+            response += '>``` info [\'stock name\'] ... - Return the summary info of the stock ```\n'
+            response += '>``` price [\'stock name\'] ... - State the current price of the stock ```\n'
+            response += '>``` graph [\'stock name\'] ... - Displays the graph of the stock ```\n'
+            response += '>``` remind [\'stock name\'] ... - Mention the user\'s and says their saved stock price every morning ```\n'
+            response += 'Please note you can have multiple stock searches if you include spaces and you must mention me'
+        elif "price" in text[1]:
+            response = "GOOG"
+        elif "info" in text[1]:
+            response = "GOOG"
+        elif "graph" in text[1]:
+            response = "GOOG"
+        elif "remind" in text[1]:
+            response = "GOOG"
+        else:
+            response = "I'm sorry I don't understand your command"
+        
+        # Sending message back to slack
+        web_client.chat_postMessage(channel=channel_id, text=response)
 
-# list of commands
-@ app.route('/help', methods=['POST'])
-def print_help():
-    client.chat_postMessage(
-        channel=channel_id, text="hello")
-    return Response(), 200
-
-
-# Get stock price
-@ app.route('/price', methods=['POST'])
-def get_stock():
-    data = request.form
-    stock_id = data.get('text_id')
-    client.chat_postMessage(
-        channel=channel_id, text="hello")
-    return Response(), 200
-
-# Get Stock Info
-@ app.route('/info', methods=['POST'])
-def get_info():
-    data = request.form
-    stock_id = data.get('text_id')
-    client.chat_postMessage(
-        channel=channel_id, text="hello")
-    return Response(), 200
-
-# Get graph Info
-@ app.route('/graph', methods=['POST'])
-def print_graph():
-    data = request.form
-    stock_id = data.get('text_id')
-    client.chat_postMessage(
-        channel=channel_id, text="hello")
-    return Response(), 200
-
-# Schedule
-
-
-# Run server if we are using this file
-if __name__ == "__main__":
-    app.run(debug=True)
+try:
+    rtm_client = RTMClient(token=os.environ['SLACK_TOKEN'])
+    print("Bot is up and running!")
+    rtm_client.start()
+except Exception as err:
+    print(err)
