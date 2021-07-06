58527fc9040a93cd60edc2047c5d357d43303bd3
davidngo123 <ngodavid215@gmail.com>
2021-07-05 18:21:41 -0700

    added in name

--- stock_api.py
+++ stock_api.py
@@ -32,31 +32,36 @@ def scrape_hist(symbol):
     soup = BeautifulSoup(page_source, 'lxml')
     return soup
 
+def get_name(soup):
+    return soup.find('h1', 'D(ib) Fz(18px)').get_text()
+
 def getPrice(stock_name):
-    print('hgelp')
     soup = scrape(stock_name)
+    name = get_name(soup)
     if(soup == "Does not exist"):
         return "I don't think this stock exists"
     price = soup.find('span', 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').get_text()
-    return 'The price of ' + stock_name + ' is $' + price + ' at this current moment.'
+    return 'The price of ' + name + ' is $' + price + ' at this current moment.'
     
 
 def getInfo(stock_name):
     soup = scrape(stock_name)
+    name = get_name(soup)
     if(soup == "Does not exist"):
         return "I don't think this stock exists"
     volume = soup.find('td', attrs={'data-test': 'TD_VOLUME-value'}).get_text()
     open  = soup.find('td', attrs={'data-test': 'OPEN-value'}).get_text()
     prev_close = soup.find('td', attrs={'data-test': 'PREV_CLOSE-value'}).get_text()
     range  = soup.find('td', attrs={'data-test': 'DAYS_RANGE-value'}).get_text()
-    sentence = 'The current volume of ' + stock_name + ' is ' +  volume + '\n'
-    sentence += 'The open value today of ' + stock_name + ' is ' +  open + '\n'
-    sentence += 'The previous close value of ' + stock_name + ' is ' +  prev_close + '\n'
-    sentence += 'The range of ' + stock_name + ' is ' + range
+    sentence = 'The current volume of ' + name + ' is ' +  volume + '\n'
+    sentence += 'The open value today of ' + name + ' is ' +  open + '\n'
+    sentence += 'The previous close value of ' + name + ' is ' +  prev_close + '\n'
+    sentence += 'The range of ' + name + ' is ' + range
     return sentence
 
 def getGraph(stock_name):
     soup = scrape_hist(stock_name)
+    name = get_name(soup)
     if(soup == "Does not exist"):
         return "I don't think this stock exists"
     dates = []
